dbbe57ce6edd8e5f953ee4ef22759e9402aa1072
Jiyong Park <jiyong@google.com>
2017-04-14 14:33:41 +0900

    marlin: copy vndk-sp libraries to /vendor/lib/vndk-sp
    Note: this shouldn't be permanent solution. Anything that is installed
    to /vendor partition must be compiled against the vndk build of the
    system libraries, which might be different than the core build of the
    libraries. However, since the number of such libraries are so many and
    thus it takes considerable time to build them as vndk. So, in the mean
    time, we just build vendor libraries against the core build as before
    and copy the core builds to the vendor partition.
    
    Test: the listed files are in /vendor/lib(64?)/vndk-sp and their
    contents are the same as those in /system/lib(64?).
    
    (cherry picked from commit 24b85fdee514e517ca635db83dfbeff56c92b242)
    
    Change-Id: I6ca53149705e3d72c43c9770210c890533ad1a18

--- device-common.mk
+++ device-common.mk
@@ -490,3 +490,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Workaround for audio glitches
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.adm.buffering.ms=3
+
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp\
+    android.hardware.graphics.allocator@2.0.vndk-sp\
+    android.hardware.graphics.mapper@2.0.vndk-sp\
+    android.hardware.graphics.common@1.0.vndk-sp\
+    android.hidl.base@1.0.vndk-sp\
+    libhwbinder.vndk-sp\
+    libbase.vndk-sp\
+    libcutils.vndk-sp\
+    libhardware.vndk-sp\
+    libhidlbase.vndk-sp\
+    libhidltransport.vndk-sp\
+    libutils.vndk-sp\
+    libc++.vndk-sp\
+    libsync.vndk-sp\
