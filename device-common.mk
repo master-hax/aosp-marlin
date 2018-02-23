049d8acb5e3348b18837532f96572f16796ece3a
Edwin Wong <edwinwong@google.com>
2018-02-22 22:18:29 -0800

    Put move_widevine_data.sh in /system/bin.
    Only include this script for device that needs to move
    licenses saved before Pi from data/mediadrm to data/vendor/mediadrm.
    
    Test: Upgrade from O-MR1 to P, Netflix & Play Movies
      can play back offline content downloaded in O-MR1
    
    Test: adb shell to verify script is in /system/bin
    
    Test: use audit2allow to verify no avc:denied for widevine drm service
    
    bug: 73656300
    Change-Id: I487d6d0b7048a7b663ce1c29fa5d3ea3cd4f3fbb

--- device-common.mk
+++ device-common.mk
@@ -154,6 +154,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.marlin
 
+# DRM HAL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/move_widevine_data.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/move_widevine_data.sh
+
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-service.widevine \
+    android.hardware.drm@1.1-service.clearkey
+
 # Audio effects
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
---
+++ move_widevine_data.sh
@@ -0,0 +1,29 @@
+#!/system/bin/sh
+
+#
+# This script needs to be copied to /system/bin.
+# The mv command preserves SELinux labels(i.e. media_data_file).
+# We need to run restorecon to put the correct context after the move.
+# However, restorecon is not implemented for /vendor/bin, so we put
+# the script in /system/bin.
+#
+
+DEST_PATH="/data/vendor/mediadrm"
+FILES_MOVED="/data/vendor/mediadrm/files_moved"
+SRC_PATH="/data/mediadrm"
+
+if [ ! -f "$FILES_MOVED" ]; then
+  for i in "$SRC_PATH/IDM"*; do
+    dest_path=$DEST_PATH/"${i#$SRC_PATH/}"
+    if [ -d "$i" ]; then
+      mkdir -p $dest_path -m 700
+      mv $i "$DEST_PATH"
+      find $dest_path -print0 | while IFS= read -r -d '' file
+        do
+          chgrp media "$file"
+        done
+    fi
+  done
+  restorecon -R "$DEST_PATH"
+  echo 1 > "$FILES_MOVED"
+fi
